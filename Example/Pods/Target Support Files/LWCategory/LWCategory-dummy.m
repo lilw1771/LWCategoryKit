a7f71f36a3b80923c7a9371de0451621eaa11caf
wang <545885919@qq.com>
2019-01-29 14:19:42 +0800

    修改分类组件

---
+++ Example/Pods/Target Support Files/LWCategory/LWCategory-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LWCategory : NSObject
+@end
+@implementation PodsDummy_LWCategory
+@end
---
+++ Example/Pods/Target Support Files/Pods-LWCategory_Tests/Pods-LWCategory_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LWCategory_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_LWCategory_Tests
+@end
